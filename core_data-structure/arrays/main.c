a8bfab65c09121350356d09d98ca0cd8e3910aba
amrxtgh <ghimireamrit619@gmail.com>
2025-09-19 13:08:55 +0545

    update

--- core_data-structure/arrays/main.c
+++ core_data-structure/arrays/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
-  return EXIT_SUCCESS;
+int main() {
+  int arr[10] = {10, 20, 30, 40, 50, 60};
+  int size = 6;
+  int pos = 3;
+  int val = 100;
+
+  for (int i = size; i > pos; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[pos] = val;
+  size++;
+
+  for (int i = 0; i < size; i++) {
+    printf("%d\n ", arr[i]);
+  }
+  printf("\n");
+  return 0;
 }
+
