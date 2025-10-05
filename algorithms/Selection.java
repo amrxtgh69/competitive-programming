8402b75fbb5592c124cf46d80415be7695c83b5b
amrxtgh <ghimireamrit619@gmail.com>
2025-10-05 20:57:25 +0545

    update

--- algorithms/Selection.java
+++ algorithms/Selection.java
@@ -1,13 +1,36 @@
 public class Selection {
-  public static void sort(Comparable[] a) {
-    int N = a.length;
-    for (int i = 0; i < N; i++) {
-      int min = i;
-      for (int j = i + 1, j < N, j++) {
-        if (less(a[j]), a[min]) min = j;
+    public static void sort(Comparable[] a) {
+      int N = a.length;
+      for (int i = 0; i < N; i++) {
+        int min = i;
+        for (int j = i + 1; j < N; j++) {
+          if (less(a[j], a[min])) min = j;
+        }
+        exch(a, i, min);
       }
-      exch(a, i, min);
     }
+      private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+      }
+      private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+      private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+          System.out.println(a[i] + " ");
+        }
+        System.out.println();
+      }
+      public static void main(String[] args) {
+        String[] a = {"pear", "apple", "orange", "banana"};
+        System.out.println("Before sorting. ");
+        show(a);
 
+        sort(a);
+
+        System.out.println("After sorting..");
+        show(a);
+      }
   }
-}
