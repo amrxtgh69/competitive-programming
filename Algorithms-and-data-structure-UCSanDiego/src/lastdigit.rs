95b003dd0300974b79bcab4f7d4eb66bec2a1915
amrxtgh <ghimireamrit619@gmail.com>
2025-12-02 01:36:58 +0545

    same

---
+++ Algorithms-and-data-structure-UCSanDiego/src/lastdigit.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin::read_line(&mut input).unwrap();
+
+    let n: u64 = input.trim().parse().unwrap();
+
+    if n <= 1 {
+        println!
+    }
+}
